b523f0bcedb90fd57841ee738153c55e5639670f
pizzetto <gabriele.murari@gmail.com>
2017-05-17 23:18:00 +0200

    Update blog-service.ts

--- src/providers/blog-service.ts
+++ src/providers/blog-service.ts
@@ -14,7 +14,7 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class BlogService {
     data;
-    private blogUrl = 'http://postazamba.shakazamba.com/api/v1/blog/';
+    private blogUrl = 'http://www.example.com/api/v1/blog/';
 
     constructor(private http: Http) {
         console.log('Hello BlogService Provider');
@@ -31,7 +31,7 @@ export class BlogService {
             // We're using Angular HTTP provider to request the data,
             // then on the response, it'll map the JSON data to a parsed JS object.
             // Next, we process the data and resolve the promise with the new data.
-            this.http.get('http://postazamba.shakazamba.com/api/v1/blog/index/megliopossibile-5')
+            this.http.get('http://www.example.com/api/v1/blog/index/blog-5')
                 .map(res => res.json())
                 .subscribe(data => {
                     // we've got back the raw data, now generate the core schedule data
